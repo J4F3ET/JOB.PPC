e7eb4bdd1215d15ecf518d6ab7eb77feabd3e8ae
J4F3ET <jslopezz@udistrital.edu.co>
2024-05-21 08:52:59 -0500

    feat: :fire: Remove responsability in execute event

--- app/src/main/java/com/example/jobcpp/View/Components/BoardView.kt
+++ app/src/main/java/com/example/jobcpp/View/Components/BoardView.kt
@@ -3,13 +3,10 @@ package com.example.jobcpp.View.Components
 
 
 import android.content.Context
-import android.view.MotionEvent
 import android.view.ViewGroup
 import android.widget.GridView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import com.example.jobcpp.Utils.MovementDirection
-import com.example.jobcpp.ViewModel.Service.EventToMovement
 import com.example.jobcpp.View.Utils.TextViewAdapter
 
 
@@ -21,10 +18,6 @@ class BoardView(
 ) {
     val grid:GridView;
     private val gap:Int = 4;
-    private var lastX:Float = 0f;
-    private var lastY:Float = 0f;
-    private val eventToMovement: EventToMovement = EventToMovement()
-
     init {
         val dimension = context.resources.displayMetrics.widthPixels.times(.8).toInt();
         val padding = 15;
@@ -40,44 +33,14 @@ class BoardView(
             background = ContextCompat.getDrawable(context, com.example.jobcpp.R.drawable.border);
             columnWidth = GridView.AUTO_FIT;
             setPadding(padding,padding,padding,padding);
-            setOnTouchListener { _, event -> eventGrid(event) };
         }
     }
-    private fun eventGrid(event: MotionEvent):Boolean{
-        return when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                this.lastX = event.x;
-                this.lastY = event.y;
-                true;
-            }
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                val movementDirection:MovementDirection? = resolveOrientationEvent(
-                    event.x - this.lastX,
-                    event.y - this.lastY
-                )
-                this.lastY = 0f;
-                this.lastX = 0f;
-                if(movementDirection is MovementDirection){
-                    this.content = eventToMovement.executeMovement(
-                        movementDirection,
-                        content
-                    ).toList()
-                }
-                true;
-            }
-            else -> false
-        }
+    fun updateContentGird(context: Context,newContent:List<Short>):GridView{
+        val items = adapterListValuesToListTextViews(context,this.content)
+        this.grid.adapter = TextViewAdapter(context,items)
+        return this.grid
     }
-    private fun resolveOrientationEvent(deltaX: Float, deltaY: Float): MovementDirection? {
-        return when {
-            deltaX > 0 && Math.abs(deltaX) > Math.abs(deltaY) -> MovementDirection.RIGHT
-            deltaX < 0 && Math.abs(deltaX) > Math.abs(deltaY) -> MovementDirection.LEFT
-            deltaY > 0 && Math.abs(deltaY) > Math.abs(deltaX) -> MovementDirection.DOWN
-            deltaY < 0 && Math.abs(deltaY) > Math.abs(deltaX) -> MovementDirection.UP
-            else -> null
-        }
-    }
-    fun adapterListValuesToListTextViews(context: Context, values:List<Short>):List<TextView>{
+    private fun adapterListValuesToListTextViews(context: Context, values:List<Short>):List<TextView>{
         val items: List<TextView> = List(values.size,) {
             ElementBoardView(
                 context,
