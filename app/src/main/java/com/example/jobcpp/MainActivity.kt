72efa1a27df4eae50cfec666256218363cd62e36
J4F3ET <jslopezz@udistrital.edu.co>
2024-05-21 08:51:25 -0500

    feat: :sparkles: Implement LiveData white mainViewModel

--- app/src/main/java/com/example/jobcpp/MainActivity.kt
+++ app/src/main/java/com/example/jobcpp/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.jobcpp
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.GridView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.jobcpp.Model.DTO.GameState
+import com.example.jobcpp.ViewModel.MainViewModel
+import com.example.jobcpp.ViewModel.Service.EventToMovement
 import com.example.jobcpp.ViewModel.Service.GeneratorGameByFunctions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainViewModel
+    private val eventToMovement: EventToMovement = EventToMovement()
+
+    private fun eventGrid(event:MotionEvent):Boolean{
+        var currentGameState: GameState = mainViewModel.observableGameState.value
+            ?: return false
+        currentGameState = eventToMovement.eventMove(event, currentGameState)
+        mainViewModel.updateGameState(currentGameState)
+        return true
+    }
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
-        val gridView: GridView = GeneratorGameByFunctions(4).generatorBoard(this);
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val containerLinearLayout:LinearLayout = findViewById(R.id.container_board)
+        val bestText:TextView = findViewById(R.id.input_text_best)
+        val scoreText:TextView = findViewById(R.id.input_text_score)
+        var gridView:GridView = mainViewModel.gridView.generatorBoard(this)
+        gridView.setOnTouchListener{_, event -> eventGrid(event)};
         containerLinearLayout.addView(gridView)
+        mainViewModel
+            .observableGameState
+            .observe(this, Observer {
+                bestText.text = it.best.toString()
+                scoreText.text = it.score.toString()
+                mainViewModel.gridView.gameState = it
+                gridView = mainViewModel.gridView.updateBoard(this,it)
+            })
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/example/jobcpp/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/jobcpp/ViewModel/MainViewModel.kt
@@ -1,14 +1,25 @@
 package com.example.jobcpp.ViewModel
 
+import android.annotation.SuppressLint
+import android.widget.GridView
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.jobcpp.Model.DTO.GameState
+import com.example.jobcpp.ViewModel.Service.EventToMovement
+import com.example.jobcpp.ViewModel.Service.GeneratorGameByFunctions
 
+@SuppressLint("ClickableViewAccessibility")
 class MainViewModel:ViewModel() {
-    val observableGameState:MutableLiveData<GameState> = MutableLiveData()
+    private val _observableGameState: MutableLiveData<GameState> = MutableLiveData()
+    val observableGameState: LiveData<GameState> get() = _observableGameState
 
-    fun eventMovement(){
-        observableGameState.value?.best = observableGameState.value?.best!! + 1
+    val gridView:GeneratorGameByFunctions = GeneratorGameByFunctions(4);
+    init {
+        _observableGameState.value = gridView.gameState
+    }
+    fun updateGameState(newGameState: GameState){
+        _observableGameState.value = newGameState
     }
     override fun onCleared() {
         super.onCleared()
